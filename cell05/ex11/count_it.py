9834d46ec1c8f476e7a0564e0971821014e6d845
chotika19 <68040129@kmitl.ac.th>
2025-06-20 05:57:11 +0000

    511

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,9 @@
+import sys
+args = sys.argv[1:]
+if len(args) == 0:
+    print("none")
+else:
+    print(f"parameters: {len(args)}")
+    for word in args:
+        print(f"{word}: {len(word)}")
+        
